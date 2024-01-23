fd8329ffc1bbb6a71c062c09750f09e10cb4a45c
Krish Patel <2021kpatel@gmail.com>
2024-01-23 10:49:28 -0500

    finished part 4 and 5 (sans the do re mi)

--- wav_spectrogram.py
+++ wav_spectrogram.py
@@ -1,12 +1,27 @@
 import matplotlib.pyplot as plt
-from scipy import signal
-from scipy.io import wavfile
+import numpy as np
+import librosa.display
 
-sample_rate, samples = wavfile.read('sound.wav')
-frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+# Get user input for the .wav file
+filename = input("Enter the .wav file name (including extension): ")
 
-plt.pcolormesh(times, frequencies, spectrogram)
-plt.imshow(spectrogram)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+# Load the audio file
+y, sr = librosa.load(filename, sr=None)
+
+# Generate the spectrogram
+D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+
+# Plot the spectrogram
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Spectrogram of ' + filename)
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+
+# Save the spectrogram as an image
+output_filename = filename.replace(".wav", "_spectrogram.png")
+plt.savefig(output_filename)
+
+# Show the spectrogram
 plt.show()
