40822b20fe52abda550563d498bc6a899cd0d118
Kyle <kerabbitt@wpi.edu>
2024-01-28 19:53:07 -0500

    change spectrogram to linear, max 8000hz, step 1000hz, sr = 44100?

--- wav_spectrogram.py
+++ wav_spectrogram.py
@@ -6,14 +6,21 @@ import librosa.display
 filename = input("Enter the .wav file name (including extension): ")
 
 # Load the audio file
-y, sr = librosa.load(filename, sr=None)
+y, sr = librosa.load(filename, sr=44100)
 
 # Generate the spectrogram
-D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+D = librosa.amplitude_to_db(np.abs(librosa.stft(y, n_fft=512)), ref=np.max)
 
 # Plot the spectrogram
 plt.figure(figsize=(14, 5))
-librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='linear')
+# Set maximum Y value to 8000Hz
+ax = plt.gca() #get current axes
+ax.set_ylim([0, 8001])
+# Get current min and max on the y axis and set them to the limits with step of 1000hz
+start, end = ax.get_ylim()
+ax.set_yticks(np.arange(start, end, 1000))
+
 plt.colorbar(format='%+2.0f dB')
 plt.title('Spectrogram of ' + filename)
 plt.xlabel('Time (s)')
