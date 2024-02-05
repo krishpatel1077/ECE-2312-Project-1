1ce849ae41dd9853e0d8f4a279e991be0ca580f2
Kyle <kerabbitt@wpi.edu>
2024-02-05 11:02:48 -0500

    added stereo delays, renamed files

--- mono_stereo.py
+++ mono_stereo.py
@@ -1,25 +1,50 @@
-import sounddevice as sd
 from scipy.io.wavfile import write
-import matplotlib.pyplot as plt
 import numpy as np
 import librosa.display
-import IPython.display as ipd
 
-filename = input("Enter the .wav file name (including extension): ")
+filename = input("Enter the .wav file name (no extension): ")
 
 # Load the audio file
-y, sr = librosa.load(filename, sr=44100)
+y, sr = librosa.load((filename + '.wav'), sr=44100)
 
-#new stereo array
-#stereo_array = np.hstack((y,y))
+#determine period
+period = 1/sr
+
+#create stereo array
 stereo_array = np.array([y,y])
-
-#convert to vertical columns of identical data
+#transpose array to be vertical
 stereo_out = np.transpose(stereo_array)
 
+#function to delay right channel by x number of ms
+
+def shift_right_channel(input_array, time_ms):
+    #convert time to amound of zeros, round and convert to int
+    num_zeros_float = np.round(time_ms/(period*1000))
+    num_zeros = num_zeros_float.astype(int)
+    #append zeros to left (top) array so both have the same size
+    shifted_array_channel_l = np.append(input_array,np.zeros(num_zeros))
+    #shift right (bottom) channel x number of zeros
+    shifted_array_channel_r = np.insert(input_array, 0, np.zeros(num_zeros))
+    #combine and transpose array to be vertical
+    transposed_stereo_out = np.array([shifted_array_channel_l,shifted_array_channel_r]).T
+    return transposed_stereo_out
+
+#delay right channel by average head
+
+#delay right channel by 1ms, 10ms, and 100ms
+#1ms
+shift_1ms = shift_right_channel(y,1)
+
+#10ms
+shift_10ms = shift_right_channel(y,10)
+
+#100ms
+shift_100ms = shift_right_channel(y,100)
 
 
-#Save to a wav file
+#Save all to a wav file
 write(f"{filename}_STEREO_CONVERT.wav", sr, stereo_out)
+write("team-stereosoundfile-1ms.wav", sr, shift_1ms)
+write("team-stereosoundfile-10ms.wav", sr, shift_10ms)
+write("team-stereosoundfile-100ms.wav", sr, shift_100ms)
 
-######VOLUME NEEDS TO BE FIXED, TOO LOW WHEN CONVERTED TO STEREO########
---
+++ record.py
@@ -0,0 +1,47 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa.display
+import IPython.display as ipd
+
+# Define Sampling Rate or Frequency in Hz
+sr = 44100
+
+# Get user input for filename and duration
+filename = input("Enter the filename (without extension): ")
+duration = float(input("Enter the duration of recording (in seconds): "))
+Mono_Stereo = int(input("Mono =1 | Stereo =2: "))
+
+# Start audio recording
+recording = sd.rec(int(duration*sr), samplerate=sr, channels=Mono_Stereo)
+# Record with a mono or stereo channel microphone
+# Record audio for the given duration
+print("Recording...............")
+sd.wait()
+# Write it to a file
+
+if Mono_Stereo == 1:
+    write(f"{filename}_MONO.wav", sr, recording)
+elif Mono_Stereo == 2:
+    write(f"{filename}_STEREO.wav", sr, recording)
+
+# Plot the recorded audio
+plt.figure(figsize=(14, 5))
+
+librosa.display.waveshow(np.array(recording[:, 0]), sr=sr, color="blue")
+plt.title('Recorded Audio')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+# Save the plot as an image
+if Mono_Stereo == 1:
+    plt.savefig(f"{filename}_soundwave_MONO.png")
+elif Mono_Stereo == 2:
+    plt.savefig(f"{filename}_soundwave_STEREO.png")
+
+# Show the plot
+plt.show()
+
+# Play the recorded audio
+ipd.Audio(f"{filename}.wav")
